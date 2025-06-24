ec2784d8a2e9c45d1c76b70ff6bebec61f977c9a
victor <victorgameplays535@gmail.com>
2025-06-24 00:25:19 -0300

    Fix: Arrumando erro de entendimento do código.

--- aula_dio_01/src/exercicio01.java
+++ aula_dio_01/src/exercicio01.java
@@ -3,22 +3,30 @@ Escreva um código que receba o nome e o ano de nascimento de alguem e imprima n
 "Olá Fulano você tem X anos."
 */
 
+import java.time.OffsetDateTime;
 import java.util.Scanner;
 
 public class exercicio01 {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         String nome;
-        int idade;
+        int nasc, idade;
+
+        int base_ano = OffsetDateTime.now().getYear();
 
         System.out.println("Digite o seu nome completo: ");
         nome = scanner.nextLine();
-        System.out.println("Digite a sua idade: ");
-        idade = scanner.nextInt();
+        System.out.println("Digite a data de nascimento: ");
+        nasc = scanner.nextInt();
 
-        System.out.printf("Olá %s, você tem %s anos de idade.", nome, idade);
+        idade = base_ano - nasc;
+
+        System.out.printf("Olá %s, você nasceu em %s e tem %s anos de idade.", nome, nasc, idade);
 
         scanner.close();
     }
