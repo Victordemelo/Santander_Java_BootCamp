340e21a687669c3e036499139ff631abcfcc35a5
victor <victorgameplays535@gmail.com>
2025-06-23 23:46:34 -0300

    Feat: Calculando a área de um quadrado.

--- aula_dio_01/src/exercicio02.java
+++ aula_dio_01/src/exercicio02.java
@@ -1,3 +1,21 @@
+/*
+Escreva um código que receba o tamanho de um lado de um quadrado, e calcule a sua área e exiba na tela.
+*/
+
+import java.util.Scanner;
+
 public class exercicio02 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float lado1, calc_area;
+
+        System.out.println("Digite o tamanho de um lado do quadrado: ");
+        lado1 = scanner.nextFloat();
+
+        calc_area = lado1 * lado1;
+        System.out.printf("A área do quadrado é %s m².", calc_area);
+
+        scanner.close();
+    }
 }
