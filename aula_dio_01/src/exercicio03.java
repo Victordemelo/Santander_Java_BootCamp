313e27784f78e2ed6b4e346681c71a6588d38b16
victor <victorgameplays535@gmail.com>
2025-06-23 23:52:16 -0300

    Feat: Calculando a base e a altura de um retângulo para exibir a sua área.

--- aula_dio_01/src/exercicio03.java
+++ aula_dio_01/src/exercicio03.java
@@ -1,3 +1,23 @@
+/*
+Escreva um código que receba a base e a altura de um retângulo, e calcule a sua área, e exiba na tela.
+*/
+
+import java.util.Scanner;
+
 public class exercicio03 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        float base, altura, calc_area;
+
+        System.out.println("Digite a base do retângulo: ");
+        base = scanner.nextFloat();
+        System.out.println("Digite a altura do retângulo: ");
+        altura = scanner.nextFloat();
+
+        calc_area = base * altura;
+        System.out.printf("A área do retângulo é %s m².", calc_area);
+
+        scanner.close();
+    }
 }
