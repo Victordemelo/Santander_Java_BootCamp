10d1d496ba8641e5bff4a222f17223274ecd5b2c
victor <victorgameplays535@gmail.com>
2025-08-13 15:07:04 -0300

    Feat: Realizando exercicio para pesquisar de valores.

---
+++ Collection_Framework_API/map/Pesquisa/Exercicio01/src/Produto.java
@@ -0,0 +1,30 @@
+public class Produto {
+
+    // Atributos
+    private final String nome;
+    private final double preco;
+    private final int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + nome + ", preco= " + preco + ", qnt= " + quantidade + "]";
+    }
+}
