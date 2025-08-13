19020836a44dfc812a08fe68ebbb54ed92ec69e1
victor <victorgameplays535@gmail.com>
2025-08-13 10:39:21 -0300

    Feat: Realizando exercicio de pesquisa utilizando o Set do collections.

---
+++ Collection_Framework_API/set/Pesquisa/Exercicio02/src/Tarefa.java
@@ -0,0 +1,59 @@
+public class Tarefa {
+
+    // Atributos
+    private String descricao;
+    private boolean concluida;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+        this.concluida = false;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + descricao + " " + concluida + "]";
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+        result = prime * result + (concluida ? 1231 : 1237);
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Tarefa other = (Tarefa) obj;
+        if (descricao == null) {
+            if (other.descricao != null)
+                return false;
+        } else if (!descricao.equals(other.descricao))
+            return false;
+        return concluida == other.concluida;
+    }
+
+}
